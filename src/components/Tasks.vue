207e6e84d38aadfa11e49b21935458f59de30bc7
Enobaks <enobakhare.emmanuel@gmail.com>
2021-08-16 23:14:43 +0100

    Initial commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrap">
+        <div v-for="task in tasks" :key="task.id">
+            <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+    name: 'Tasks',
+    props: ['tasks'],
+    components: {
+        Task
+    }
+}
+</script>
+
+<style>
+
+</style>
